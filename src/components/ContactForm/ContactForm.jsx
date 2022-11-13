55dfe4eb23c1ee29e600dfe8502c1bdb796503cb
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-11-13 18:25:19 +0100

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,58 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import style from './ContactForm.module.css';
 
+export const ContactForm = ({ handleSubmit }) => {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    let id = nanoid();
 
-const INITIAL_STATE = {
-    id: '',
-    name: '',
-    number: ''
-};
-
-export class ContactForm extends Component {
-    state = { ...INITIAL_STATE };
-    id = nanoid();
-
-    handleChange = e => {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
+    const handleName = e => {
+        const { value } = e.target;
+        setName(value);
     };
 
-    handleSubmit = e => {
+    const handleNumber = e => {
+        const { value } = e.target;
+        setNumber(value);
+    };
+
+    const handleFormSubmit = e => {
         e.preventDefault();
-        this.props.onSubmit({ ...this.state });
-        this.reset();
+        const form = e.currentTarget;
+        handleSubmit({ name: name, number: number });
+        form.reset();
     };
 
-    reset = () => this.setState({ ...INITIAL_STATE });
-
-    render() {
-        return (
-            <form className={style.form} onSubmit={this.handleSubmit}>
-                <label htmlFor={this.id}>Name</label>
-                <input className={style.input}
-                    id={this.id}
-                    onChange={this.handleChange}
-                    value={this.state.name}
-                    type="text"
-                    name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                />
-                <label htmlFor={this.id}>Phone</label>
-                <input className={style.input}
-                    id={this.id}
-                    type="tel"
-                    name="number"
-                    value={this.state.number}
-                    onChange={this.handleChange}
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                />
-                <button className={style.btn} type="submit">Add contact</button>
-            </form>
-        );
-    };
+    return (
+        <form className={style.form} onSubmit={handleFormSubmit}>
+            <label htmlFor={id}>Name</label>
+            <input
+                className={style.input}
+                id={id}
+                onChange={handleName}
+                value={name}
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                placeholder='Enter name'
+                required
+            />
+            <label htmlFor={id}>Phone</label>
+            <input
+                className={style.input}
+                id={id}
+                type="tel"
+                name="number"
+                value={number}
+                onChange={handleNumber}
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                placeholder='Enter number'
+                required
+            />
+            <button className={style.btn} type="submit">
+                Add contact
+            </button>
+        </form>
+    );
 };
